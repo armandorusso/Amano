510178e9a3dafcfab351055505c9b12e1e1583ca
Armando Russo <russo_449@hotmail.com>
2023-03-22 23:37:45 -0400

    Add Moveable Spikes/Obstacles that move from 1 point to another

---
+++ Amano/Assets/Items/MoveableObstacle.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObstacle : MonoBehaviour
+{
+    [SerializeField] private Transform _point1;
+    [SerializeField] private Transform _point2;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _delayBetweenPoints;
+    
+    private Transform _endPoint;
+    private Transform _newPoint;
+    
+    private void Start()
+    {
+        _endPoint = _point2;
+        _newPoint = _point1;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, _endPoint.position, _speed);
+        if (Vector2.Distance(transform.position, _endPoint.position) <= 0.5f)
+        {
+            var temp = _endPoint;
+            _endPoint = _newPoint;
+            _newPoint = temp;
+            // Coroutine instead?
+        }
+    }
+}
