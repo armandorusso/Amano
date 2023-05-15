204c3fb3dc5b38b39a9b8ff592b7e2fca8bb0410
Armando Russo <russo_449@hotmail.com>
2023-05-14 21:50:15 -0400

    Implement code for trampoline animation

--- Amano/Assets/Objects/Trampoline.cs
+++ Amano/Assets/Objects/Trampoline.cs
@@ -8,11 +8,27 @@ public class Trampoline : MonoBehaviour
     [SerializeField] private float _jumpForceAmount;
     [SerializeField] private Vector2 _launchDirection;
 
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Player") && col.gameObject.TryGetComponent(out Rigidbody2D rb))
+        if (col.gameObject.CompareTag("Player"))
         {
-            rb.velocity += _launchDirection * _jumpForceAmount;
+            col.rigidbody.velocity += _launchDirection * _jumpForceAmount;
+            _animator.SetBool("hasSteppedOn", true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            _animator.SetBool("hasSteppedOn", false);
         }
     }
 }
